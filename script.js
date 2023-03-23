4791c478fc0decd7c5039048c02d623102d03565
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-23 11:05:43 +0800

    added animation

---
+++ script.js
@@ -0,0 +1,7 @@
+const emailBtn = document.querySelector('.email-btn')
+
+let typed = new Typed('#element', {
+  strings: ['Millen Mark is a web developer and UI/UX designer who creates visually appealing and user-friendly digital experiences. With extensive experience gained through coding bootcamp, Millen has honed his proficiency in coding and design to create dynamic and responsive websites that impress clients. He stays up-to-date with the latest industry trends and technologies and is eager to apply his skills to an entry-level position where he can continue to learn and grow.'],
+  typeSpeed: 1,
+  startDelay: 2000,
+});
