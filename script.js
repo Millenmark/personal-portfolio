3dd8af2b1494a1a07e7b8efd1792e7a8ced200be
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-06-09 10:47:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ heroTimeline.from(".hero__illustration", {
   y: 30,
   opacity: 0,
 });
+
+heroTimeline.from(".hero__illustration__image", {
+  rotate: -90,
+  repeat: -1,
+  yoyo: true,
+  duration: 15,
+});
+
+gsap.from(".hero__illustration__text", {
+  y: "-115px",
+  repeat: -1,
+  duration: 10,
+  yoyo: true,
+  opacity: 1,
+  delay: 1,
+});
